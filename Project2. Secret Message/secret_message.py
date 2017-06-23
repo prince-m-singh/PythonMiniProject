e77392ddbbfdc23a72d2a51e3ac3460f96b2fc74
Prince Kumar <prince.m.singh11@gmail.com>
2017-06-23 20:45:25 +0530

    Secret message project done

---
+++ Project2. Secret Message/secret_message.py
@@ -0,0 +1,15 @@
+import os
+def rename_files():
+	file_list=os.listdir(r".\prank\prank")
+	print (file_list)
+	current_path=os.getcwd()
+	print current_path
+	os.chdir(r".\prank\prank")
+
+	for file_name in file_list:
+		print "Old file name", file_name
+		print "new file name", os.rename(file_name,file_name.translate(None,"0123456789"))
+	os.chdir(current_path)	
+
+rename_files()
+
