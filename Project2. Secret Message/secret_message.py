bff27ed513738bc1bb74c235d6593fc574842fba
Prince Kumar <prince.m.singh11@gmail.com>
2017-06-23 22:31:33 +0530

    turtlrs project completed

--- Project2. Secret Message/secret_message.py
+++ Project2. Secret Message/secret_message.py
@@ -8,8 +8,8 @@ def rename_files():
 
 	for file_name in file_list:
 		print "Old file name", file_name
-		print "new file name", os.rename(file_name,file_name.translate(None,"0123456789"))
+		print "new file name",   os.rename(file_name,file_name.translate(None,"0123456789"))
 	os.chdir(current_path)	
-
+ 
 rename_files()
 
